f163a799b7cab6473795e84a11e450f660c73c53
Sun_pig <806746185@qq.com>
2019-07-02 00:35:43 +0800

    add kafkaTemplate demo

--- src/test/java/com/study/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/com/study/kafkademo/KafkaDemoApplicationTests.java
@@ -1,16 +1,20 @@
 package com.study.kafkademo;
 
+import com.study.kafkademo.producer.Producer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KafkaDemoApplicationTests {
+    @Autowired
+    private Producer producer;
 
-    @Test
     public void contextLoads() {
+        producer.sendMessage("test", "topic--------test");
+        producer.sendMessage("test2", "topic--------test2");
     }
-
 }
